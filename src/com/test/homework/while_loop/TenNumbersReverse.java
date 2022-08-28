dfd95dc2e5c117b61cfec233f37bef5f52040c58
radelitsca <radelitsca@gmail.com>
2022-08-28 14:46:05 +0300

    add TenNumbersReverse

---
+++ src/com/test/homework/while_loop/TenNumbersReverse.java
@@ -0,0 +1,15 @@
+package com.test.homework.while_loop;
+
+public class TenNumbersReverse {
+    /*
+    Print numbers from 10 to 1
+    using loop while
+     */
+    public static void main (String[] args){
+        int i = 10;
+        while (i>0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
