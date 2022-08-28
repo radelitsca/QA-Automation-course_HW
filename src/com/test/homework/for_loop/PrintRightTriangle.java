c49c5f294716f28f77136d84929c8490a35a9a99
radelitsca <radelitsca@gmail.com>
2022-08-28 14:43:14 +0300

    add TenNumbers

--- src/com/test/homework/for_loop/PrintRightTriangle.java
+++ src/com/test/homework/for_loop/PrintRightTriangle.java
@@ -12,7 +12,7 @@ public class PrintRightTriangle {
     8888
     .....
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
                 for (int i = 1; i <= 10; i++) {
                    PrintSeveralTimes(i);
                 }
