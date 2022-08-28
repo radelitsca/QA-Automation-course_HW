7d55a640a0d1e3087297a8b70d3fddf31cf1e8b6
radelitsca <radelitsca@gmail.com>
2022-08-28 13:06:52 +0300

    MinOfTwo

---
+++ src/com/test/homework/if_else/MinOfTwo.java
@@ -0,0 +1,25 @@
+package com.test.homework.if_else;
+
+import java.util.Scanner;
+
+public class MinOfTwo {
+    /*
+    Enter and reed two numbers from the console
+    Implement method which prints the lowest variable
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        printMinOfTwo(one, two);
+        scanner.close();
+    }
+
+    private static void printMinOfTwo(int a, int b) {
+        if (a>b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
